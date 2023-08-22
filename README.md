472af7f94ff978b87f1228ca74b8a165941fc52e
hellofromtonya <tonya.mork@automattic.com>
2023-08-22 17:06:11 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Font Face tester plugin
+
+This tester plugin tests printing an array of fonts that are passed to `wp_print_font_faces()`.
+
+## How to install
+
+To clone the repo:
+1. In your local dev environment, create a new folder in `wp-content/plugins/` called `font-face-tester`.
+2. Then type: `git clone https://github.com/hellofromtonya/font-face-tester.git`.
+3. Then activate the plugin in your test site's admin area.
+
+To download a zip:
+1. Click to download the zip version of this repo.
+2. Then in your local dev environment, go to Plugins > Add New > and then select the downloaded zip to add.
+3. Then activate the plugin in your test site's admin area.
---
+++ font-face-tester.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin Name: Font Face Tester
+ * Plugin URI: https://github.com/wordpress/gutenberg
+ * Description: Tests passing an array of fonts to Font Face.
+ * Requires at least: trunk
+ * Requires PHP: 7.0
+ * Version: 1.0.0
+ */
+
+add_action( 'wp_head', 'fontfacetester_print_fonts', 50 );
+add_action( 'admin_print_styles', 'fontfacetester_print_fonts', 50 );
+function fontfacetester_print_fonts() {
+	$url   = plugin_dir_url( __FILE__ );
+	$fonts = array(
+		'Playfair Display' => array(
+			array(
+				'font-family' => 'Playfair Display',
+				'font-style'   => 'normal',
+				'font-weight'  => '200 900',
+				'src'          => array(
+					$url . 'assets/fonts/PlayfairDisplay-VariableFont_wght.ttf',
+				),
+			),
+			array(
+				'font-family' => 'Playfair Display',
+				'font-style'   => 'italic',
+				'font-weight'  => '200 900',
+				'src'          => array(
+					$url . 'assets/fonts/PlayfairDisplay-Italic-VariableFont_wght.ttf',
+				),
+			),
+		),
+	);
+	wp_print_font_faces( $fonts );
+}
